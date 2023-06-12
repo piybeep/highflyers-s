0b7a4a30477aad887ce6b2577392c199a49c69bd
Даня <45875910+danyatochkaru@users.noreply.github.com>
2023-06-13 00:41:42 +0300

    fix: убрал добавление refreshToken в куки

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,8 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  Req,
-  Res,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { AuthDto } from './dto/auth.dto';
-import { Request, Response } from 'express';
+import { Request } from 'express';
 import { AccessTokenGuard } from 'src/common/guards/accessToken.guard';
 import { RefreshTokenGuard } from 'src/common/guards/refreshToken.guard';
 import { ApiBody, ApiTags } from '@nestjs/swagger';
@@ -22,23 +14,13 @@ export class AuthController {
 
   @Post('signup')
   @ApiBody({ type: CreateUserDto })
-  async signup(
-    @Body() createUserDto: CreateUserDto,
-    @Res({ passthrough: true }) res: Response,
-  ) {
-    const data = await this.authService.signUp(createUserDto);
-    res.cookie('session', data.refreshToken, { httpOnly: true, secure: false });
-    return data;
+  async signup(@Body() createUserDto: CreateUserDto) {
+    return this.authService.signUp(createUserDto);
   }
 
   @Post('signin')
-  async signin(
-    @Body() authDto: AuthDto,
-    @Res({ passthrough: true }) res: Response,
-  ) {
-    const data = await this.authService.signIn(authDto);
-    res.cookie('session', data.refreshToken, { httpOnly: true, secure: false });
-    return data;
+  async signin(@Body() authDto: AuthDto) {
+    return await this.authService.signIn(authDto);
   }
 
   @UseGuards(AccessTokenGuard)

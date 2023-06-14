35da9be1f079ee406a5dc606f27805b3ccbeec9e
Даня <45875910+danyatochkaru@users.noreply.github.com>
2023-06-15 00:33:08 +0300

    docs: услуги * добавил связь с категориями * добавил описание в документации к обновлению услуги

--- src/services/dto/update-service.dto.ts
+++ src/services/dto/update-service.dto.ts
@@ -1,4 +1,44 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
 import { CreateServiceDto } from './create-service.dto';
+import {
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsPositive,
+  IsString,
+  IsUUID,
+} from 'class-validator';
 
-export class UpdateServiceDto extends PartialType(CreateServiceDto) {}
+export class UpdateServiceDto extends PartialType(CreateServiceDto) {
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNotEmpty({ message: 'Отсутствует имя услуги' })
+  @IsString({ message: 'Имя услуги должно быть строкой' })
+  name?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNotEmpty({ message: 'Отсутствует цена услуги' })
+  @IsInt({ message: 'Цена должна быть целочисленным числом' })
+  @IsPositive({ message: 'Цена должна быть больше 0' })
+  defaultPrice?: number;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNotEmpty({ message: 'Отсутствует цена со скидкой услуги' })
+  @IsInt({ message: 'Цена со скидкой должна быть целочисленным числом' })
+  @IsPositive({ message: 'Цена должна быть больше 0' })
+  discountPrice?: number;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNotEmpty({ message: 'Отсутствуют опции услуги' })
+  @IsString({ each: true })
+  points?: string[];
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNotEmpty({ message: 'Отсутствует идентификатор категории' })
+  @IsUUID('4')
+  categoryId: string;
+}

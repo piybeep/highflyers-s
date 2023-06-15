3bab5e5a1daf030a28195968cf6f197f30b16de9
Даня <45875910+danyatochkaru@users.noreply.github.com>
2023-06-15 18:59:03 +0300

    fix: импорт категории в услуги

--- src/services/services.module.ts
+++ src/services/services.module.ts
@@ -3,9 +3,10 @@ import { ServicesService } from './services.service';
 import { ServicesController } from './services.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Service } from './entities/service.entity';
+import { CategoriesModule } from '../categories/categories.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Service])],
+  imports: [TypeOrmModule.forFeature([Service]), CategoriesModule],
   controllers: [ServicesController],
   providers: [ServicesService],
 })

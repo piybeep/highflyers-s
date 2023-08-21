5ce4914b9a9036a442d1870cd7659033caf7a0c2
Даня <45875910+danyatochkaru@users.noreply.github.com>
2023-08-21 20:43:54 +0300

    fix: зарегистрировал планы уроков в TypeOrm

--- src/lesson-plans/lesson-plans.module.ts
+++ src/lesson-plans/lesson-plans.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LessonPlansService } from './lesson-plans.service';
 import { LessonPlansController } from './lesson-plans.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LessonPlan } from './entities/lesson-plan.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([LessonPlan])],
   controllers: [LessonPlansController],
   providers: [LessonPlansService],
 })

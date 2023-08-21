85308a05c2925769388adc5f3ff2c249fe81f923
Даня <45875910+danyatochkaru@users.noreply.github.com>
2023-08-21 20:40:16 +0300

    fix: зарегистрировал уровни в TypeOrm

--- src/levels/levels.module.ts
+++ src/levels/levels.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LevelsService } from './levels.service';
 import { LevelsController } from './levels.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Level } from './entities/level.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Level])],
   controllers: [LevelsController],
   providers: [LevelsService],
   exports: [LevelsService],

2fc43605770248f524b8d4e271aada0dfa3d7cc4
Даня <45875910+danyatochkaru@users.noreply.github.com>
2023-08-22 16:57:37 +0300

    fix: добавил в import LevelsModule

--- src/lesson-plans/lesson-plans.module.ts
+++ src/lesson-plans/lesson-plans.module.ts
@@ -3,9 +3,10 @@ import { LessonPlansService } from './lesson-plans.service';
 import { LessonPlansController } from './lesson-plans.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LessonPlan } from './entities/lesson-plan.entity';
+import { LevelsModule } from '../levels/levels.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([LessonPlan])],
+  imports: [TypeOrmModule.forFeature([LessonPlan]), LevelsModule],
   controllers: [LessonPlansController],
   providers: [LessonPlansService],
 })

6097c61eef14c868fa28c6a9a362115102d8dcde
Даня <45875910+danyatochkaru@users.noreply.github.com>
2023-08-29 15:42:01 +0300

    fix: подключил slugmodule

--- src/tags/tags.module.ts
+++ src/tags/tags.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Tag } from '@src/tags/entities/tag.entity';
+import { SlugModule } from 'nestjs-slug';
 import { TagsController } from './tags.controller';
 import { TagsService } from './tags.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Tag])],
+    imports: [TypeOrmModule.forFeature([Tag]), SlugModule],
     controllers: [TagsController],
     providers: [TagsService],
     exports: [TagsService],
